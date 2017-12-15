10ff7ec3587cb56455b321b3b21239f5d4e84d6c
feng <764523295@qq.com>
2017-12-15 17:50:38 +0800

    update

---
+++ laravel_demo/app/Http/Controllers/Home/TestController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Home;
+
+use App\Http\Controllers\Controller;
+
+class TestController extends Controller
+{
+	public function index()
+	{
+		echo 321;
+	}
+}
